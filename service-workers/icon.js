49be0f901c2d20972e90388cce10ca238d3a25e8
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-07-21 18:55:06 +0200

    Fixed wrong icon being displayed in navigation bar until slider is toggled off and on once.

--- service-workers/icon.js
+++ service-workers/icon.js
@@ -2,18 +2,18 @@ function updateIcon() {
   chrome.storage.sync.get(["isActive"], function (result) {
     if (result.isActive === undefined) {
       chrome.storage.sync.set({ isActive: true }, function () {
+        chrome.action.setIcon({ path: "/graphics/nav_bar.png" });
         console.log("sdl: isActive was undefined, set to true by default.");
       });
-    }
-  });
-
-  chrome.storage.sync.get(["isActive"], function (result) {
-    if (result.isActive) {
-      chrome.action.setIcon({ path: "/graphics/nav_bar.png" });
     } else {
-      chrome.action.setIcon({ path: "/graphics/nav_bar_inactive.png" });
+      if (result.isActive) {
+        chrome.action.setIcon({ path: "/graphics/nav_bar.png" });
+      } else {
+        chrome.action.setIcon({ path: "/graphics/nav_bar_inactive.png" });
+      }
     }
   });
 }
-
 updateIcon();
+chrome.runtime.onInstalled.addListener(updateIcon);
+chrome.runtime.onStartup.addListener(updateIcon);
